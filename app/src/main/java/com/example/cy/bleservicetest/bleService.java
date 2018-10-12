fdca74234033e050cfc4e4ea50f508cd6faad790
cy <chihying.phone@gmail.com>
2018-10-12 12:25:49 +0800

    add gatt server add service and characteristics

--- app/src/main/java/com/example/cy/bleservicetest/bleService.java
+++ app/src/main/java/com/example/cy/bleservicetest/bleService.java
@@ -3,8 +3,12 @@ package com.example.cy.bleservicetest;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.AdvertiseCallback;
@@ -13,7 +17,9 @@ import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.ParcelUuid;
 import android.util.Log;
 import android.widget.Toast;
@@ -31,7 +37,18 @@ public class bleService extends Service {
 
     private Set<BluetoothDevice> mConnectedDevices = new HashSet<>();
 
-    public static UUID MY_BT_SERVICE_UUID = UUID.fromString("0000A101-0000-1000-8000-00805f9b34fb");
+    byte[] readResp = new byte[1];
+
+    byte[] readWriteResp = new byte[1];
+
+    byte[] readWriteNotifyResp = new byte[1];
+
+    public static final UUID MY_BT_SERVICE_UUID = UUID.fromString("0000A101-0000-1000-8000-00805f9b34fb");
+    public static final UUID MY_BT_CHAR_FOR_READ_UUID = UUID.fromString("0000AAA0-0000-1000-8000-00805f9b34fb");
+    public static final UUID MY_BT_CHAR_FOR_READ_WRITE_UUID = UUID.fromString("0000AAA1-0000-1000-8000-00805f9b34fb");
+    public static final UUID MY_BT_CHAR_FOR_READ_WRITE_NOTIFY_UUID = UUID.fromString("0000AAA2-0000-1000-8000-00805f9b34fb");
+
+    public static UUID CLIENT_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
 
     Boolean isBleSupported;
@@ -57,6 +74,10 @@ public class bleService extends Service {
         // We can't continue without proper Bluetooth support
         isBleSupported = checkBleSupport(bluetoothAdapter);
 
+        readResp[0]=0;
+        readWriteResp[0]=0;
+        readWriteNotifyResp[0]=0;
+
         super.onCreate();
     }
 
@@ -67,7 +88,7 @@ public class bleService extends Service {
             Log.e(TAG, "BLE is not supported! Do nothing...");
         }else{
             startAdvertising();
-            //startServer();
+            startServer();
         }
         return super.onStartCommand(intent, flags, startId);
     }
@@ -76,12 +97,23 @@ public class bleService extends Service {
     public void onDestroy() {
         Log.i(TAG, "onDestroy()");
 
-        //stopServer();
+        stopServer();
         stopAdvertising();
 
         super.onDestroy();
     }
 
+
+    private void showToast(String toastMsg){
+
+        final String m = toastMsg;
+        new Handler(Looper.getMainLooper()).post(new Runnable(){
+            public void run(){
+                Toast.makeText(getApplicationContext(), m, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
 
     private BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
 
@@ -90,13 +122,75 @@ public class bleService extends Service {
             if (newState == BluetoothProfile.STATE_CONNECTED) {
                 Log.i(TAG, "BluetoothDevice CONNECTED: " + device);
                 mConnectedDevices.add(device);
-                Toast.makeText(getApplicationContext(),"BluetoothDevice CONNECTED:" + device, Toast.LENGTH_LONG).show();
+                showToast("BluetoothDevice CONNECTED:");
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 Log.i(TAG, "BluetoothDevice DISCONNECTED: " + device);
                 //Remove device from any active subscriptions
                 mConnectedDevices.remove(device);
-                Toast.makeText(getApplicationContext(),"BluetoothDevice DISCONNECTED:" + device, Toast.LENGTH_LONG).show();
+                showToast("BluetoothDevice DISCONNECTED:");
             }
+            super.onConnectionStateChange(device, status, newState);
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicReadRequest(device, requestId, offset, characteristic);
+
+            if(MY_BT_CHAR_FOR_READ_UUID.equals(characteristic.getUuid()) ){
+                readResp[0] += 1;
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        readResp);
+            }
+
+            if(MY_BT_CHAR_FOR_READ_WRITE_UUID.equals(characteristic.getUuid())){
+                readWriteResp[0] += 2;
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        readWriteResp);
+            }
+
+            if(MY_BT_CHAR_FOR_READ_WRITE_NOTIFY_UUID.equals(characteristic.getUuid())){
+                readWriteNotifyResp[0] +=3;
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        readWriteNotifyResp);
+            }
+        }
+
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite, responseNeeded, offset, value);
+            Log.d(TAG, "onCharacteristicWriteRequest");
+
+
+            if(MY_BT_CHAR_FOR_READ_WRITE_UUID.equals(characteristic.getUuid()) ){
+                Log.d(TAG, "uuid: " + MY_BT_CHAR_FOR_READ_WRITE_UUID);
+                Log.d(TAG, "device: " + device);
+                Log.d(TAG, "requestId: " + requestId);
+                Log.d(TAG, "characteristic: " + characteristic);
+                Log.d(TAG, "preparedWrite: " + preparedWrite);
+                Log.d(TAG, "responseNeeded: " + responseNeeded);
+                Log.d(TAG, "offset: " + offset);
+                Log.d(TAG, "value: " + value);
+
+                readWriteResp[0] = value[0];
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        readWriteResp);
+            }
+
+
+
 
         }
     };
@@ -105,6 +199,7 @@ public class bleService extends Service {
         @Override
         public void onStartSuccess(AdvertiseSettings settingsInEffect) {
             Log.i(TAG, "LE Advertise Started.");
+            showToast("LE Advertise Started.");
         }
 
         @Override
@@ -122,10 +217,10 @@ public class bleService extends Service {
         }
 
         AdvertiseSettings settings = new AdvertiseSettings.Builder()
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
                 .setConnectable(true)
                 .setTimeout(0)
-                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
                 .build();
 
         AdvertiseData data = new AdvertiseData.Builder()
@@ -143,7 +238,7 @@ public class bleService extends Service {
         mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
     }
 
-    /*
+
     private void startServer() {
         mBluetoothGattServer = mBluetoothManager.openGattServer(this, mGattServerCallback);
         if (mBluetoothGattServer == null) {
@@ -151,15 +246,58 @@ public class bleService extends Service {
             return;
         }
 
-        mBluetoothGattServer.addService(TimeProfile.createTimeService());
-
+        mBluetoothGattServer.addService(getMyBleService());
     }
     private void stopServer() {
         if (mBluetoothGattServer == null) return;
 
         mBluetoothGattServer.close();
     }
+
+
+    public static BluetoothGattService getMyBleService() {
+
+        //service
+        BluetoothGattService service = new BluetoothGattService(MY_BT_SERVICE_UUID,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY);
+
+        //characteristic 1: for read
+        BluetoothGattCharacteristic forRead = new BluetoothGattCharacteristic(MY_BT_CHAR_FOR_READ_UUID,
+                //Read-only characteristic
+                BluetoothGattCharacteristic.PROPERTY_READ ,
+                BluetoothGattCharacteristic.PERMISSION_READ);
+/*        forRead.addDescriptor(
+                new BluetoothGattDescriptor(CLIENT_CONFIG,
+                    //Read-only descriptor
+                    BluetoothGattDescriptor.PERMISSION_READ));
 */
+        //characteristic 2: for write
+        BluetoothGattCharacteristic forReadWrite = new BluetoothGattCharacteristic(MY_BT_CHAR_FOR_READ_WRITE_UUID,
+                //Read-only characteristic
+                BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE,
+                BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
+/*        forReadWrite.addDescriptor(
+                new BluetoothGattDescriptor(CLIENT_CONFIG,
+                    //Read/write descriptor
+                    BluetoothGattDescriptor.PERMISSION_READ | BluetoothGattDescriptor.PERMISSION_WRITE));
+*/
+        //characteristic 3: for write
+        BluetoothGattCharacteristic forReadWriteNotify = new BluetoothGattCharacteristic(MY_BT_CHAR_FOR_READ_WRITE_NOTIFY_UUID,
+                //Read-only characteristic
+                BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_WRITE | BluetoothGattCharacteristic.PROPERTY_NOTIFY,
+                BluetoothGattCharacteristic.PERMISSION_READ | BluetoothGattCharacteristic.PERMISSION_WRITE);
+        /*forReadWriteNotify.addDescriptor(
+                new BluetoothGattDescriptor(CLIENT_CONFIG,
+                        //Read/write descriptor
+                        BluetoothGattDescriptor.PERMISSION_READ | BluetoothGattDescriptor.PERMISSION_WRITE));
+*/
+
+        service.addCharacteristic(forRead);
+        service.addCharacteristic(forReadWrite);
+        service.addCharacteristic(forReadWriteNotify);
+
+        return service;
+    }
 
 
     private boolean checkBleSupport(BluetoothAdapter bluetoothAdapter) {
